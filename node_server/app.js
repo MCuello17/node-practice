85d78e31c1f422244984e0cff9fe378708664a30
Michael Cuello <mjcuello17@gmail.com>
2021-06-23 10:37:20 -0300

    Deleted directory
    Messed up with branches, someday I'll get used to github...

--- node_server/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const routes = require('./routes');
-
-const server = http.createServer(routes);
-
-const port = 3000;
-server.listen(port);
-console.log(`Listening on port ${port}`);
